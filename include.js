3d8e40fd21a2e474effbbad4431586156a795098
Justin O'Boyle <justinjoboyle@me.com>
2015-09-23 17:52:41 -0400

    Correction: include.js is minified

--- include.js
+++ include.js
@@ -1,1 +1,24 @@
-var include={};include.injector={};include.settings={};include.settings.injectInto="head";include.srcLink="https://github.com/justinoboyle/include.js";include.author="Justin O'Boyle & GitHub Community at "+include.srcLink;include.loadScript=function(c){var d=document.createElement("script");d.type="text/javascript";d.src=c;include.injector.inject(d);return d};include.loadStylesheet=function(c){var d=document.createElement("link");d.type="text/css";d.href=c;d.media="screen";include.injector.inject(d);return d};include.injector.inject=function(b){document.getElementsByTagName(include.settings.injectInto)[0].appendChild(b)};
+var include = {};
+include.injector = {};
+include.settings = {};
+include.settings.injectInto = "head";
+include.srcLink = "https://github.com/justinoboyle/include.js";
+include.author = "Justin O'Boyle & GitHub Community at " + include.srcLink;
+include.loadScript = function(c) {
+    var d = document.createElement("script");
+    d.type = "text/javascript";
+    d.src = c;
+    include.injector.inject(d);
+    return d
+};
+include.loadStylesheet = function(c) {
+    var d = document.createElement("link");
+    d.type = "text/css";
+    d.href = c;
+    d.media = "screen";
+    include.injector.inject(d);
+    return d
+};
+include.injector.inject = function(b) {
+    document.getElementsByTagName(include.settings.injectInto)[0].appendChild(b)
+};
